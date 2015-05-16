e352f4d2be7d412770186b6d8442288e9a30bf68
bryhlee <bhlee@scu.edu>
2015-05-15 18:36:11 -0700

    update

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 /* Inti WOW.js */
 new WOW().init();
+
 /* Smooth Scroll Links */
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
@@ -15,13 +16,37 @@ $(function() {
     }
   });
 });
+
 /* Text Rotation */
 $(".rotate").textrotator({
   animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
   separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
   speed: 2000 // How many milliseconds until the next word show.
 });
+
 /* Preloader */
 $(window).load( setTimeout( function() {
 	$(".loader").fadeOut("slow");
 },400)); 
+
+/* Owl Carousel */
+$(document).ready(function() {
+ 
+  $("#slider").owlCarousel({
+ 
+      // navigation : true, // Show next and prev buttons
+      autoPlay: 5000, //Set AutoPlay to 3 seconds
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
