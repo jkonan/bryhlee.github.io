2e1c3eca40ddf2c9b67786a41d88af3f42e2ca34
bryhlee <bhlee@scu.edu>
2015-05-15 17:55:36 -0700

    Added carousel and fixed customjs

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+/* Inti WOW.js */
+new WOW().init();
+/* Smooth Scroll Links */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+/* Text Rotation */
+$(".rotate").textrotator({
+  animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  speed: 2000 // How many milliseconds until the next word show.
+});
+/* Preloader */
+$(window).load( setTimeout( function() {
+	$(".loader").fadeOut("slow");
+},400)); 
